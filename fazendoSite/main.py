9fe273a01c4f6ce82989d2b2b48d291c6859b291
AwsumName03 <th.damasceno@hotmail.com>
2022-06-25 21:44:50 -0300

    ImAlterada a API para funcionar junto com o front-end

--- fazendoSite/main.py
+++ fazendoSite/main.py
@@ -1,11 +1,13 @@
 from website import criar_app
-
+import mypokeAPI
 
 def main():
     app = criar_app()
-
     app.run(debug=True)
 
 
 if __name__ == "__main__":
+    mypokeAPI.deletar_base_de_dados()
+    mypokeAPI.criar_base_de_dados()
+    mypokeAPI.popularBD()
     main()
--- fazendoSite/website/views.py
+++ fazendoSite/website/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
+from . import db
 
 views = Blueprint('views', __name__)
 
