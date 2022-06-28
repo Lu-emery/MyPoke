c037622ae5e3837c3ba8f006a322089b5c17f98a
Luemery <luemery@live.com>
2022-06-27 23:00:53 -0300

    Adição do Readme + rename da pasta do site

---
+++ src/website/models.py
@@ -0,0 +1,9 @@
+from . import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), unique=True)
+    password = db.Column(db.String(40))
+    database = db.Column(db.String(40))
+    is_admin = db.Column(db.Boolean, default=False)
